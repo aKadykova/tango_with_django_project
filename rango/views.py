b69457f5e75bf53110ffe7fd5190d871eb4f0ea2
Anastasiia Kadykova <2829507K@student.gla.ac.uk>
2023-02-06 23:54:18 +0000

    Chapter 9

--- rango/views.py
+++ rango/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
-from django.shortcuts import redirect
 from rango.forms import PageForm
 from django.shortcuts import redirect
 from django.urls import reverse
+from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -17,9 +20,11 @@ def index(request):
     context_dict['pages'] = pages_list
     return render(request, 'rango/index.html', context=context_dict)
 
+
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Anastasiia'}
     return render(request, 'rango/about.html', context=context_dict)
+
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -79,3 +84,64 @@ def add_page(request, category_name_slug):
 
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
+
+
+def register(request):
+     registered = False
+
+     if request.method == 'POST':
+         user_form = UserForm(request.POST)
+         profile_form = UserProfileForm(request.POST)
+
+         if user_form.is_valid() and profile_form.is_valid():
+             user = user_form.save()
+
+             user.set_password(user.password)
+             user.save()
+
+             profile = profile_form.save(commit=False)
+             profile.user = user
+
+             if 'picture' in request.FILES:
+                 profile.picture = request.FILES['picture']
+
+             profile.save()
+
+             registered = True
+         else:
+             print(user_form.errors, profile_form.errors)
+
+     else:
+         user_form = UserForm()
+         profile_form = UserProfileForm()
+
+     return render(request, 'rango/register.html',context = {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+def user_login(request):
+    if request.method== 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username = username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -6,6 +6,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -115,3 +116,6 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 #Media files
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+#LOGIN page
+LOGIN_URL = 'rango:login'
